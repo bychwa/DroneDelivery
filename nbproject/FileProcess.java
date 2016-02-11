2e8a3c2f890283bfb4a8b8b4529913b3cf6e7104
agazi <agaziberihu@gmail.com>
2016-02-11 20:50:19 +0100

    Fiel processing

---
+++ nbproject/FileProcess.java
@@ -0,0 +1,15 @@
+package nbproject;
+import java.io.*;
+import java.util.*;
+
+public class FileProcess {
+	public static void main() {
+		try {
+			File file1 = new File ("busy_day.in");
+			Scanner scn = new Scanner (file1);
+			while()
+		} catch (FileNotFoundException fnfe){
+		System.out.println("File not found");}
+		 
+	}
+}
