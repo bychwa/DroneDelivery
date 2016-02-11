a076b1dcfb9fb1e064a55b79efb3c09ff662ee26
agazi <agaziberihu@gmail.com>
2016-02-11 19:37:22 +0100

    Product class

---
+++ Product.java
@@ -0,0 +1,18 @@
+
+public class Product {
+	private int weight;
+	private int productType;
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getProductType() {
+		return productType;
+	}
+	public void setProductType(int productType) {
+		this.productType = productType;
+	}
+	
+}
