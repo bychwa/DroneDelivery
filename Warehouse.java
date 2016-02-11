405850302aad90fec9d971121c86e6c246ef3736
agazi <agaziberihu@gmail.com>
2016-02-11 19:45:12 +0100

    Warehouse class

---
+++ Warehouse.java
@@ -0,0 +1,20 @@
+
+public class Warehouse {
+	private int row;
+	private int column;
+	//It should able to store some products, i.e connection
+	// with Product class
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+}
