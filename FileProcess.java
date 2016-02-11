6415270cf0b120f4ada82f58b580f7c1119f917a
Jackson Isack <jacksonisack@gmail.com>
2016-02-11 21:05:12 +0100

    File processing

--- FileProcess.java
+++ FileProcess.java
@@ -1,18 +1,25 @@
-
 import java.io.*;
 import java.util.*;
 
 public class FileProcess {
-	public static void main() {
-		try {
-			File file1 = new File ("busy_day.in");
-			Scanner scn = new Scanner (file1);
-			while(scn.hasNextLine()){
-				System.out.println(scn.nextLine());
-			}
-			
-		} catch (FileNotFoundException fnfe){
-		System.out.println("File not found");}
+	public static void main(String[] args) {
+            try {
+                    File file1 = new File ("busy_day.in");
+                    Scanner scn = new Scanner (file1);
+                    int count=0;
+                    while(scn.hasNextLine()){
+                        count++;
+                        if(count==1){
+                            String Line =scn.nextLine();
+                            String[] splited = Line.split("\\s+");    
+                            System.out.println("row "+splited[0]+" col "+splited[1]);
+                            
+                        }
+                        
+                    }
+
+            } catch (FileNotFoundException fnfe){
+            System.out.println("File not found");}
 		 
 	}
 }
