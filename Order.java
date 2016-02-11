06a5418bac1f84b0e5e05cfd1d3385ec6fe89bfb
Jackson Isack <jacksonisack@gmail.com>
2016-02-11 19:54:44 +0100

    Drone + updated Order Class

--- Order.java
+++ Order.java
@@ -12,26 +12,34 @@ public class Order {
     
     private Product products[];
     private Location deliveredLocation;
+    private String status;
     
     public Order(Product products[],Location del){
             this.products=products;
             this.deliveredLocation=del;
     }
 
-	public Product[] getProducts() {
-		return products;
-	}
+    public Product[] getProducts() {
+            return products;
+    }
 
-	public void setProducts(Product[] products) {
-		this.products = products;
-	}
+    public void setProducts(Product[] products) {
+            this.products = products;
+    }
 
-	public Location getDeliveredLocation() {
-		return deliveredLocation;
-	}
+    public Location getDeliveredLocation() {
+            return deliveredLocation;
+    }
 
-	public void setDeliveredLocation(Location deliveredLocation) {
-		this.deliveredLocation = deliveredLocation;
-	}
+    public void setDeliveredLocation(Location deliveredLocation) {
+            this.deliveredLocation = deliveredLocation;
+    }
+
+    public void setStatus(String status){
+        this.status=status;
+    }
+    public String getStatus(){
+        return status;
+    }
     
 }
